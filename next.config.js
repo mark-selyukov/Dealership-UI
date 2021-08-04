053f4179a6730d33f00ca93877f98cd6c1decb14
Mark's Laptop <superiorseven421@gmail.com>
2021-08-04 17:00:23 -0500

    Config was added

---
+++ next.config.js
@@ -0,0 +1,31 @@
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require("next/constants");
+
+// This uses phases as outlined here: https://nextjs.org/docs/#custom-configuration
+module.exports = (phase) => {
+  // when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environmental variable
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  // when `next build` or `npm run build` is used
+  const isProd =
+    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== "1";
+  // when `next build` or `npm run build` is used
+  const isStaging =
+    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === "1";
+
+  if (isDev) {
+    console.log("Dev deployment");
+  } else if (isStaging) {
+    console.log("Staging deployment");
+  } else if (isProd) {
+    console.log("Prod deployment");
+  }
+
+  // next.config.js object
+  return {
+    env: {
+      DEALERSHIPAPI: process.env.DEALERSHIPAPI,
+    },
+  };
+};
